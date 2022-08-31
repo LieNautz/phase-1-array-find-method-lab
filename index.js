985eb59297027b8584929b274a33310da08d0539
LieNautz <tuan_lai01@hotmail.com>
2022-08-31 21:31:03 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+
+// //   returns a year the KC Chiefs won the superbowl
+//   function superbowlWin(record) {
+//     let yearWon = record.find(record => record.result === "W");
+//     return yearWon ? yearWon.year : undefined;
+//   }
+
+
+function superbowlWin(record) {
+    let winningYear = record.find(record => record.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
